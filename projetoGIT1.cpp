6866024f2d8b0ddcfe6c731b43daa125d6e9632c
mylena21 <myyliioutlook@gmail.com>
2019-11-21 16:23:17 -0300

    implementei 2 funcoes

--- projetoGIT1.cpp
+++ projetoGIT1.cpp
@@ -44,7 +44,13 @@ string substituirLetra(char letra1,char letra2, string texto)
 */
 int contaPalavras(string palavra, string texto)
 {
-
+    size_t pos = texto.palavra(word, 0);
+    while(pos != string::npos)
+    {
+        cnt++;
+        pos = texto.palavra(word, pos+1);
+}
+    return pos;
 
 }
 
@@ -53,6 +59,13 @@ int contaPalavras(string palavra, string texto)
 */
 string substituirPalavras(string palavra1, string palavra2, string texto)
 {
+    size_t pos = texto.find(palavra1);
+    while(pos != string::npos)
+    {
+        texto.replace(pos,palavra1.length(),palavra2);
+        pos = texto.find(palavra1);
+    }
+    return texto;
 
 
 }
@@ -61,5 +74,26 @@ int main()
 {
 
 	cout << "Entre com o texto para fazer o teste:";
+	cout<< "Digite letra:";
+	cin.get(letra);
+	cin.get(lixo);
+	cout<< "Digite texto:";
+	getline(cin,texto);
+	qtdl=(letra,lixo);
+	cout<<"Digite primeira letra:";
+	cin.get(letra1);
+	cin.get(lixo);
+	cout<<"Digite segunda letra:";
+	cin.get(letra2);
+	cin.get(lixo);
+	rtxt=(letra1,letra2,texto);
+	cout<<"Digite palavra:";
+	getline(cin,palavra);
+	qtdls=(palavra,texto);
+	cout<<"Digite primeira palavra:";
+	getline(cin,palavra1);
+	cout<<"Digite segunda palavra:";
+	getline(cin,palavra2);
+	subs=(palavra1,palavra2,texto);
     return 0;
 }
